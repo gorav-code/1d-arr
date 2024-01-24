bc59526012e1ef324a497da7a56cfcbf4582e695
gorav-code <gorav.code@outlook.com>
2024-01-24 11:28:30 +0530

    using previous index value
    Shortening the code and using previous index value of array rather creating new array to save output. because we will no longer be using previous index array value. therefore we can use it to override last sum value.

--- main.cpp
+++ main.cpp
@@ -5,16 +5,6 @@
 const int MAX = 5;
 int currentSum = 0;
 
-int getSum(int arr[], int size)
-{
-    int sum = 0;
-    for (int i = 0; i < size; i++)
-    {
-        sum = sum + arr[i];
-    } 
-    return sum;
-}
-
 void printArr(std::string tag, int arr[], int size)
 {
     std::cout << "\n" << tag << " [";
@@ -28,19 +18,31 @@ void printArr(std::string tag, int arr[], int size)
     std::cout << "]";
 }
 
+int getSum(int arr[], int size)
+{
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum = sum + arr[i];
+    } 
+    return sum;
+}
+
+
 int main()
 {
     int arr[MAX] = {3, 1, 2, 10, 1};
-    int output[MAX];
-
     printArr("Input: ", arr, MAX); 
 
     for (int i = 0; i < MAX; i++)
     {
-        output[i] = getSum(arr, i + 1);
+        if(i > 0){
+            //since we are not using previous value anymore in our program then we can simply replace previous index value.
+            arr[i] += arr[i-1];
+        }
     }
 
-    printArr("Output: ", output, MAX); 
+    printArr("Output: ", arr, MAX); 
 
     //time complexity = O(n)
     //space complexity = O(1)
